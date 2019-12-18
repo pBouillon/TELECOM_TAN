7424cf3bb81150c6879065dfa3d7da03188d0450
Pierre Bouillon <pro.pierre.bouillon@protonmail.com>
2019-12-18 11:43:01 +0100

    🌱 Initialize project structure
    Co-authored-by: Thomas BAGREL <tomsb07@gmail.com>

---
+++ __main__.py
@@ -0,0 +1,5 @@
+"""
+:authors: [Thomas Bagrel, Pierre Bouillon, Florian Vogt]
+:url: https://github.com/pBouillon/TELECOM_TAN
+:license: [MIT](https://github.com/pBouillon/TELECOM_TAN/blob/master/LICENSE)
+"""
