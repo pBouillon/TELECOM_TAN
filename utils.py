391634d8f4258c7b4e111e5cd13c6fe60aa6f3ed
Pierre Bouillon <pro.pierre.bouillon@protonmail.com>
2019-12-18 14:33:05 +0100

    ✨ FFT Spectrum
    Co-authored-by: Thomas BAGREL <tomsb07@gmail.com>

--- utils.py
+++ utils.py
@@ -6,13 +6,14 @@
     :license: [MIT](https://github.com/pBouillon/TELECOM_TAN/blob/master/LICENSE)
 """
 
-from typing import NamedTuple
 from enum import Enum
+from typing import NamedTuple
 from os import path
+from math import pi
 
+import matplotlib.pyplot as plt
 import numpy as np
 import scipy.io.wavfile as wavfile
-import matplotlib.pyplot as plt
 
 
 """Expected wav file rate
@@ -27,7 +28,7 @@ NU = 13
 """
 N = 2 ** NU
 
-"""Sampling period
+"""Sample spacing
 """
 T_E = 1 / CD_QUALITY_RATE
 
@@ -37,8 +38,11 @@ T = N * T_E
 
 """Time vector
 """
-TV = np.linspace(0, T, N, endpoint=False)
+TV = np.linspace(-T / 2, T / 2, N, endpoint=False)
 
+"""N-shifted vector 
+"""
+NSV = np.arange(0, N, 1)
 
 class Phonem(Enum):
     """Phonem enum to classify phonems
@@ -56,7 +60,7 @@ class Phonem(Enum):
     U = 'u'
 
 
-class RawSample(NamedTuple):
+class Sample(NamedTuple):
     """.wav data object
     """
 
@@ -73,7 +77,47 @@ class RawSample(NamedTuple):
     data: np.array
 
 
-def load_wav_file(file_path: str) -> RawSample:
+class Spectrum(NamedTuple):
+    """TODO
+    """
+    
+    """Sample value
+    """
+    data: np.array
+    
+    """Sample frequency
+    """
+    freq: np.array
+    
+    """Source of the original recording
+    """
+    file_name: str
+    
+    """Handled Phonem
+    """
+    phonem: Phonem
+
+
+def spectrum(sample: Sample) -> Spectrum:
+    """Apply the discrete Fourier Transform on the current sample
+
+    :remark: Spectrum.data only contains the modulus of the fft spectrum
+
+    :param sample: audio sample to work on
+
+    :returns: the generated Spectrum object
+    """
+    data = np.abs(np.fft.fft(sample.data))
+    freq = np.fft.fftfreq(N, T_E)
+    return Spectrum(
+        data=data,
+        file_name=sample.file_name,
+        freq=freq,
+        phonem=sample.phonem
+    )
+
+
+def load_wav_file(file_path: str) -> Sample:
     """Loads a wav file and returns the corresponding RawSample data object.
 
     :param file_path: path to the wav file
@@ -97,5 +141,31 @@ def load_wav_file(file_path: str) -> RawSample:
     phonem = Phonem(phonem_str_caps.lower())
 
     # Instanciate the associated data object
-    return RawSample(phonem, file_path, data)
+    return Sample(phonem, file_path, data)
 
+
+def window(sample: Sample) -> Sample:
+    """Fetch the hamming window for the current sample
+
+    :param sample: audio sample to work on
+
+    :returns: the sample with the the Hamming window applied on its data
+    """
+    hamming_window = (1 / 2) * (1 + np.cos(2 * pi * (NSV - N / 2) / N))
+    return Sample(
+        phonem=sample.phonem,
+        file_name=sample.file_name,
+        data=hamming_window * sample.data
+    )
+
+
+def main():
+    raw_sample = load_wav_file("/home/thomas/Bureau/samples/A/a_fvogt_1.wav")
+    w = window(raw_sample)
+    s = spectrum(w)
+    plt.plot(s.freq, s.data)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
